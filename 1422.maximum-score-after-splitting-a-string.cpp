28fd8d8ef255b9e16889089ef10f19cad25c1aca
kuma0416 <a24350845@gmail.com>
2025-01-01 17:13:23 +0800

    feat: Push the Q.1422 with yellow notation

---
+++ 1422.maximum-score-after-splitting-a-string.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    int maxScore(string inputStr) {
+        int onesCount = 0;
+        int zerosCount = 0;
+        int bestScore = INT_MIN;
+
+        for (int i = 0; i < inputStr.size() - 1; i++) {
+            if (inputStr[i] == '1') {
+                onesCount++;
+            } else {
+                zerosCount++;
+            }
+            
+            bestScore = max(bestScore, zerosCount - onesCount);
+        }
+        
+        if (inputStr[inputStr.size() - 1] == '1') {
+            onesCount++;
+        }
+        
+        return bestScore + onesCount;
+    }
+};
+// other solutions
+class Solution {
+public:
+    int maxScore(string inputStr) {
+        int onesCount = 0;
+        int zerosCount = 0;
+        int bestScore = INT_MIN;
+
+        for (int i = 0; i < inputStr.size() - 1; i++) {
+            if (inputStr[i] == '1') {
+                onesCount++;
+            } else {
+                zerosCount++;
+            }
+            cout << zerosCount << " " << onesCount << " " << bestScore << "\n";
+            bestScore = max(bestScore, zerosCount - onesCount);
+        }
+        
+        if (inputStr[inputStr.size() - 1] == '1') {
+            onesCount++;
+        }
+        
+        return bestScore + onesCount;
+    }
+};
