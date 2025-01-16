41010bb1441f723fd68a1348dd1048ffbc1664e1
kuma0416 <a24350845@gmail.com>
2025-01-16 18:15:59 +0800

    feat: Push the Q.2425 with green notation

---
+++ 2425.bitwise-xor-of-all-pairings.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    int xorAllNums(vector<int>& nums1, vector<int>& nums2) {
+        int n1Len=nums1.size(), n2Len=nums2.size();
+        int ans=0;
+        if(n1Len%2==0 && n2Len%2==0){
+            return ans;
+        } else if(n1Len%2!=0 && n2Len%2==0){
+            for(int i: nums2){
+                ans ^= i;
+            }
+            return ans;
+        } else if(n1Len%2==0 && n2Len%2!=0){
+            for(int i: nums1){
+                ans ^= i;
+            }
+            return ans;
+        } else {
+            for(int i: nums1){
+                ans ^= i;
+            }
+            for(int i: nums2){
+                ans ^= i;
+            }
+            return ans;
+        }
+    }
+};
+// other solution
+class Solution {
+public:
+    int xorAllNums(vector<int>& nums1, vector<int>& nums2) {
+        int c1 = nums1.size();
+        int c2 = nums2.size();
+        int x1 = 0, x2 = 0;
+        
+        if (c1 % 2 != 0) {
+            for (int num : nums2) {
+                x2 ^= num;
+            }
+        }
+        if (c2 % 2 != 0) {
+            for (int num : nums1) {
+                x1 ^= num;
+            }
+        }
+        return x1 ^ x2;
+    }
+};
